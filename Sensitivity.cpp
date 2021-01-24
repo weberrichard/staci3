41c58dc1cece840038a560fd9c9c94b3589b8fe2
HuzsvarTamas <huzsvar.tamas@gmail.com>
2021-01-24 19:59:56 +0100

    Sensitivity upgrade -> a few useful function
    A few practical function in given into the sensitivity calculator.

--- Sensitivity.cpp
+++ Sensitivity.cpp
@@ -29,6 +29,21 @@ bool Sensitivity::calculateSensitivity(string parameter)
         massFlowRateSensitivity.col(i) = resultDerivative.head(numberEdges);
         pressureSensitivity.col(i) = resultDerivative.tail(numberNodes);
       }
+      for (int i = 0; i < numberEdges; ++i)
+      {
+        for (int j = 0; j < numberEdges; ++j)
+        {
+           rowSum += massFlowRateSensitivity(i,j);
+        }
+        if(abs(rowSum) > abs(rowSumLargest))
+        {
+          rowSumLargest = rowSum;
+          setLargestSensitivity(rowSumLargest);
+        }
+        cout << "rowSum" << rowSum << endl;
+        edges[i]->setMFRSens(rowSum);
+        rowSum = 0.;
+      }
     }
     else if(strcmp(parameter.c_str(), "demand") == 0)
     {
@@ -43,6 +58,20 @@ bool Sensitivity::calculateSensitivity(string parameter)
         massFlowRateSensitivity.col(i) = resultDerivative.head(numberEdges);
         pressureSensitivity.col(i) = resultDerivative.tail(numberNodes);
       }
+      for (int i = 0; i < numberNodes; ++i)
+      {
+        for (int j = 0; j < numberNodes; ++j)
+        {
+           rowSum += pressureSensitivity(i,j);
+        }
+        if(abs(rowSum) > abs(rowSumLargest))
+        {
+          rowSumLargest = rowSum;
+          setLargestSensitivity(rowSumLargest);
+        }
+        nodes[i]->setPsens(rowSum);
+        rowSum = 0.;
+      }
     }
     else
     {
@@ -56,4 +85,98 @@ bool Sensitivity::calculateSensitivity(string parameter)
     cout << endl << "[*] Sensitivity (" << parameter << "): hydraulic solver has NOT convergenved :(" << endl;
 
   return convergence;
+}
+
+void Sensitivity::fillEdgeSensitivityForPlot(double OverrideMax)
+{
+  int numberEdges = edges.size();
+  double rowSum, rowSumLargest = OverrideMax;
+  cout << "max sens: " << rowSumLargest << endl;
+  for (int i = 0; i < numberEdges; ++i)
+  {
+    rowSum = edges[i]->getMFRSens();
+    edges[i]->setMFRSens(rowSum/rowSumLargest);
+  }
+}
+
+void Sensitivity::fillPipeSensitivityForPlot()
+{
+  int numberEdges = edges.size();
+  double rowSum, rowSumLargest = getLargestSensitivity();
+  cout << "max sens: " << rowSumLargest << endl;
+  for (int i = 0; i < numberEdges; ++i)
+  {
+    if(edges[i]->type == "Pipe")
+    {
+      rowSum = edges[i]->getMFRSens();
+      edges[i]->setMFRSens(abs(rowSum/rowSumLargest));
+      edges[i]->setDoubleProperty("MFRSensitivity",abs(rowSum/rowSumLargest));
+      cout <<"rowSum2: " << edges[i]->getMFRSens() << " : " << edges[i]->getDoubleProperty("MFRSensitivity") << endl;
+    }
+  }
+}
+
+void Sensitivity::fillEdgeSensitivityForPlot()
+{
+  int numberEdges = edges.size();
+  double rowSum, rowSumLargest = getLargestSensitivity();
+  cout << "max sens: " << rowSumLargest << endl;
+  for (int i = 0; i < numberEdges; ++i)
+  {
+    rowSum = edges[i]->getMFRSens();
+    edges[i]->setMFRSens(abs(rowSum/rowSumLargest));
+    cout <<"rowSum2: " <<  edges[i]->getMFRSens() << endl;
+  }
+}
+
+void Sensitivity::fillNodalSensitivityForPlot()
+{
+  int numberNodes = nodes.size();
+  double rowSum, rowSumLargest = getLargestSensitivity();
+  cout << "max sens: " << rowSumLargest << endl;
+  for (int i = 0; i < numberNodes; ++i)
+  {
+    rowSum = nodes[i]->getPsens();
+    nodes[i]->setPsens(rowSum/rowSumLargest);
+  }
+}
+
+void Sensitivity::fillNodalSensitivityForPlotNotNormalized()
+{
+  int numberNodes = nodes.size();
+  double rowSum;
+  //cout << "max sens: " << rowSumLargest << endl;
+  for (int i = 0; i < numberNodes; ++i)
+  {
+    rowSum = nodes[i]->getPsens();
+    nodes[i]->setPsens(rowSum);
+  }
+}
+
+double Sensitivity::CalculateAverageSensitivity()
+{
+  int numberNodes = nodes.size();
+  double rowsum = 0.;
+  double nodal = 0.;
+  double AverageLocal;
+  for (int i = 0; i < numberNodes; ++i)
+  {
+    rowsum = nodes[i]->getPsens();
+    //cout << "node " << i << ". loc. sens: " << rowsum << endl; 
+    nodal += rowsum;
+  }
+  AverageLocal = nodal/numberNodes;
+  //cout << " Av. Local: " << AverageLocal << endl;
+  return AverageLocal;
+}
+
+void Sensitivity::fillNodalSensitivityForPlot(double OverrideMax)
+{
+  int numberNodes = nodes.size();
+  double rowSum, rowSumLargest = OverrideMax;
+  for (int i = 0; i < numberNodes; ++i)
+  {
+    rowSum = nodes[i]->getPsens();
+    nodes[i]->setPsens(rowSum/rowSumLargest);
+  }
 }
--- Sensitivity.h
+++ Sensitivity.h
@@ -23,6 +23,30 @@ public:
   MatrixXd massFlowRateSensitivity; // Sensitivity Matrix
   MatrixXd pressureSensitivity; // Sensitivity Matrix
   
+  MatrixXd getPSensMatrix() // It makes reachable the nodal part of the sens. matrix.
+  {
+    return pressureSensitivity;
+  }
+  MatrixXd getMFRSensMatrix() // It makes reachable the link section of the sens. matrix.
+  {
+    return massFlowRateSensitivity;
+  }
+  double getLargestSensitivity() // It makes reachable the largest NODAL sensitivity
+  {
+    return LargestSensitivity;
+  }
+  void setLargestSensitivity(double a) // This function modifies the maximal NODAL sensitivity, it should be used only in the cases, when something modifies the sensitivity matrix.
+  {
+    LargestSensitivity = a;
+  }
+  void fillPipeSensitivityForPlot(); // It gives for the pipes the calculated MFR sensitivity values (not normalized by the maximal sensitivity)
+  void fillEdgeSensitivityForPlot(); // It gives all of the edges a specific sensitivity values, Pools and Pressure points also have sensitivity -> 0
+  void fillEdgeSensitivityForPlot(double OverrideMax); // It gives all of the edges a specific sensitivity values, Pools and Pressure points also have sensitivity -> 0, but the values are normalized with a given maximal value.
+  void fillNodalSensitivityForPlot(); // It gives for the nodes the calculated pressure sensitivity values (normalized by the maximal sensitivity)
+  void fillNodalSensitivityForPlot(double OverrideMax); // It gives for the nodes the calculated pressure sensitivity values (normalized by the GIVEN maximal sensitivity)
+  void fillNodalSensitivityForPlotNotNormalized(); // It gives for the nodes the calculated pressure sensitivity values (not mormalized by the maximal sensitivity)
+  double CalculateAverageSensitivity(); // It calculates the average value of the NODAL part of the sensitivity matrix, which means all of the rows are summarized and average row sum value is calculated.
+
 	Sensitivity(string spr_filename);
 	~Sensitivity();
 
@@ -30,7 +54,8 @@ public:
       Paramter can be either Pipe diameter, roughness or Node demand.
       diameter | friction_coeff | demand */
 	bool calculateSensitivity(string parameter);
-	
+private:
+  double LargestSensitivity = 0.0;  // The maximal value of the NODAL sensitivity.
 };
 
 #endif
